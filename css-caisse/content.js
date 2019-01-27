14ae6b20a0c6b162eea15c6d7a15592e0d4d06ad
Rodrigo Osorio <rodrigo.osorio@honeywell.com>
2019-01-27 12:50:12 +0100

    find element by class and add some space before the button

--- css-caisse/content.js
+++ css-caisse/content.js
@@ -1,2 +1,2 @@
-var el = document.getElementById("tmpl_header_right");
-el.innerHTML += '<button type="button" onclick="if (confirm(\'Fermer la caisse ?\')) { window.location=\'http://127.0.0.1:8080/stopkiosk\'; }">Fermer la caisse</button>';
+var el = document.getElementsByClassName("o_database_list")[0];
+el.innerHTML += '<center><br><br><p><button type="button" onclick="if (confirm(\'Fermer la caisse ?\')) { window.location=\'http://127.0.0.1:8080/stopkiosk\'; }">Fermer la caisse</button></p></center>';
